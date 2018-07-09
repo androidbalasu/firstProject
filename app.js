8f9fbc8dacd0362284bc08076b31d02bbafdcfa1
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-07-09 14:43:14 -0500

    Example using node's OS module.  Provides the ability to show system info which is not available in t a browser environment.

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const path = require ('path');
+const os = require('os');
 
-var pathObj = path.parse(__filename);
+var totalmemory = os.totalmem();
+var freememory = os.freemem();
 
-console.log(pathObj);
+console.log('Total memory '+ totalmemory);
+console.log('Free memory '+ freememory);
+
+//Template string
+//ES6 / ES2015 : ECMAScript 6
+
+console.log(`Total memory using template string: ${totalmemory}`);
+console.log(`Free memory using template string: ${freememory}`);
